c3cbbd498d422a15e5be744d2f0c5eac3a04188c
James <hello@jmes.tech>
2020-02-18 20:11:58 +0000

    🐛 fix: pagination (#41)
    * 🐛 fix: pagination
    
    * 🐛 fix: canonical

--- __tests__/paginate.spec.js
+++ __tests__/paginate.spec.js
@@ -6,6 +6,8 @@ describe('Paginate', () => {
   it('should return correct params when index is undefined', () => {
     const result = paginate(undefined, offset, 100)
     expect(result).toEqual({
+      hasNext: true,
+      hasPrevious: false,
       previousIndex: 0,
       nextIndex: 20,
       totalResults: 100,
@@ -15,8 +17,10 @@ describe('Paginate', () => {
   it('should return correct params when index is larger than total results', () => {
     const result = paginate(100, offset, 80)
     expect(result).toEqual({
+      hasNext: false,
+      hasPrevious: true,
       previousIndex: 60,
-      nextIndex: 0,
+      nextIndex: 80,
       totalResults: 80,
     })
   })
@@ -24,6 +28,8 @@ describe('Paginate', () => {
   it('should return correct params when there are more results to view', () => {
     const result = paginate(0, offset, 100)
     expect(result).toEqual({
+      hasNext: true,
+      hasPrevious: false,
       previousIndex: 0,
       nextIndex: 20,
       totalResults: 100,
@@ -34,8 +40,10 @@ describe('Paginate', () => {
   it('should return correct params when all results have been viewed', () => {
     const result = paginate(100, offset, 100)
     expect(result).toEqual({
+      hasNext: false,
+      hasPrevious: true,
       previousIndex: 80,
-      nextIndex: 0,
+      nextIndex: 100,
       totalResults: 100,
     })
   })
